36c1e8d25e3ce8ab3dd6c24498441349d2f1abb2
StitchNG <stitchnig@gmail.com>
2019-06-10 21:45:41 +0100

    Create instructions.js

---
+++ instructions.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/**
+ * adonis-mixpanel
+ *
+ * @license MIT
+ * @copyright Slynova - Romain Lanz <romain.lanz@slynova.ch>
+ * @extended Oparand - Ifeora Okechukwu <isocroft@gmail.com> | Aziz Abdul <>
+ */
+
+const path = require('path')
+
+module.exports = async function (cli) {
+  try {
+    await cli.makeConfig('mixpanel.js', path.join(__dirname, 'config/mixpanel.js'))
+    cli.command.completed('create', 'config/mixpanel.js')
+  } catch (error) {
+    // ignore if mixpanel.js already exists...
+  }
+}
